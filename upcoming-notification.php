72e7f6095eca81b78be3bb946fc74e9a178600bc
Bernard Bolter <bemajor@Bernards-iMac.local>
2016-12-05 22:46:19 -0800

    artist ibio image adjustment

--- upcoming-notification.php
+++ upcoming-notification.php
@@ -5,6 +5,6 @@ $the_link = get_permalink( $upcoming_exhibition[0]->ID);
  ?>
 
 <div class="upcoming" id="upcoming">
-  <p class="upcoming__info"><span class="upcoming__hide">New! </span>Upcoming show info can be found <a href="<?php echo $the_link; ?>">here</a></p>
+  <p class="upcoming__info"><span class="upcoming__hide">New! </span> Upcoming show info can be found <a href="<?php echo $the_link; ?>">here</a></p>
   <a class="upcoming__close" id="upcoming-close" href="#">x</a>
 </div>
